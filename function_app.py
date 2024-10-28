2c68bc34747f1550fa36f92d6d3c16796dd209d0
itay <nudeli@myumanitoba.ca>
2024-10-28 18:24:20 -0500

    Added azure function for tracking APYs

--- function_app.py
+++ function_app.py
@@ -7,6 +7,7 @@ from io import StringIO, BytesIO
 from azure.storage.blob import BlobServiceClient
 import os
 import time
+from datetime import date
 
 app = func.FunctionApp()
 
@@ -45,7 +46,8 @@ def get_token_balances(headers,address,network):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         
-        existing_data = retrieve_csv_from_blob()
+        blob_name = "defi-tracker.csv" 
+        existing_data = retrieve_csv_from_blob(blob_name)
         data = response.json()
         
         new_data = {
@@ -72,16 +74,15 @@ def get_token_balances(headers,address,network):
 
             updated_df = new_df
 
-        save_csv_to_blob(updated_df)
+        save_csv_to_blob(updated_df,blob_name)
 
     except requests.RequestException as e:
         print(f"Request error: {e}")
 
-def retrieve_csv_from_blob():
+def retrieve_csv_from_blob(blob_name):
     try:
         
         container_name = "defi-tracker"
-        blob_name = "defi-tracker.csv"  
 
         blob_service_client = BlobServiceClient.from_connection_string(os.environ['AzureWebJobsStorage'])
         container_client = blob_service_client.get_container_client(container_name)
@@ -101,11 +102,10 @@ def retrieve_csv_from_blob():
         logging.error(f"Error retrieving file from Blob Storage: {e}")
         return None
 
-def save_csv_to_blob(df):
+def save_csv_to_blob(df, blob_name):
     try:
 
         container_name = "defi-tracker"
-        blob_name = "defi-tracker.csv" 
 
         # Create a BlobServiceClient object
         blob_service_client = BlobServiceClient.from_connection_string(os.environ['AzureWebJobsStorage'])
@@ -121,3 +121,131 @@ def save_csv_to_blob(df):
 
     except Exception as e:
         logging.error(f"Error uploading file to Blob Storage: {e}")
+
+@app.timer_trigger(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def CheckAPYs(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    url = 'https://public-api.de.fi/graphql/'  # Replace this with the actual De.Fi GraphQL endpoint
+    try:
+
+        chainsQuery = """
+        query {
+            chains {
+                id
+                absoluteChainId
+                abbr
+                name
+                type
+
+                }
+            }
+        """
+        # Define the GraphQL query
+        query = """
+        {
+        opportunities {
+            id
+            chainId
+            apr
+            totalValueLocked
+            categories
+            investmentUrl
+            isNew
+            status
+            farm {
+            id
+            url
+            slug
+            logo
+            categories
+            }
+            tokens {
+            borrowRewards {
+                address
+                displayName
+                icon
+                symbol
+                name
+            }
+            deposits {
+                address
+                displayName
+                icon
+                symbol
+                name
+            }
+            rewards {
+                address
+                displayName
+                icon
+                symbol
+                name
+            }
+            }
+        }
+        }
+
+        """
+        
+        # Set up the request headers
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Api-Key': '75ad57b4233940e38e45c2c72e827f9c'  # Replace with your actual API key if required
+        }
+        
+        responseChains = requests.post(
+            url,
+            json={'query': chainsQuery},
+            headers=headers
+        )
+        
+        chainsDict = {}
+        dataChains = responseChains.json()
+        for chain in dataChains['data']['chains']:
+            chainsDict[chain['id']] = chain['name']
+        
+        time.sleep(10)
+        
+        # Make the request
+        response = requests.post(
+            url,
+            json={'query': query},
+            headers=headers
+        )
+        
+        blob_name = "APY-tracker.csv" 
+        existing_data = retrieve_csv_from_blob(blob_name)
+        data = response.json()
+        
+        new_data = {
+            'Defi Protocol': [],
+            'Network': [],
+            'Updated At': [],
+            'Token': [],
+            'APY': []
+        }
+        opportunities = data['data']['opportunities']
+        for opportunity in opportunities:
+            if (any(s in opportunity['categories'] for s in ["lending", "stablecoin"])):
+                new_data['Network'].append(chainsDict[int(opportunity['chainId'])])
+                new_data['APY'].append(opportunity['apr'] * 100)
+                new_data['Defi Protocol'].append(opportunity['farm']['slug'])
+                new_data['Token'].append(opportunity['tokens']['deposits'][0]['displayName'])
+                new_data['Updated At'].append(date.today())
+        
+        new_df = pd.DataFrame(new_data)
+        
+        if existing_data is not None:
+            updated_df = pd.concat([existing_data, new_df], ignore_index=True)
+        else:
+
+            updated_df = new_df
+        
+        save_csv_to_blob(updated_df,blob_name)
+    
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
